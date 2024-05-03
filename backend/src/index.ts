d723a67d5d8bb104d3ed42738ba2f4889e007693
Marcelo <mhbcoelho99@gmail.com>
2024-05-03 11:33:14 -0300

    resolving some bugs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,11 @@
-import express, { Application } from 'express';
-import userRoutes from './routes/userRoutes';
+import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 const port: Number = 4444;
 
 app.use(express.json());
-app.use('/api', userRoutes);
+//app.use('/api', userRoutes);
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('hello World!');
 });
 
