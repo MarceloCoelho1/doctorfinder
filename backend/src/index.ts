3663bb8fec4c48d258a1ad759a7b047104d0d91d
Marcelo <mhbcoelho99@gmail.com>
2024-05-06 08:00:42 -0300

    adding sequelize and a simple example: how to create an user model with sequelize and typescript

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 const app: Application = express();
 const port: Number = 4444;
 
 app.use(express.json());
+app.use(cors());
 //app.use('/api', userRoutes);
 
 app.get('/', (req: Request, res: Response) => {
---
+++ backend/src/models/UserExample.ts
@@ -0,0 +1,45 @@
+// isso aqui é um exemplo de model criando com typescript e sequelize
+// o erro que ta dando na linha 18 é pq n configurei o banco de dados ainda, ignorem.
+
+import { Model, DataTypes } from 'sequelize';
+// import { sequelize } from '../database'; almir vai configurar dps xD
+
+interface UserAttributes {
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+interface UserInstance extends Model<UserAttributes>, UserAttributes {}
+
+const User = sequelize.define<UserInstance>('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  tableName: 'users', // Nome da tabela no banco de dados
+});
+
+export default User;
